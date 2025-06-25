615e06332e1312baa853a5f6ae4f2eec072f43c0
Bolormaa Tsogt <boloroots47@gmail.com>
2025-06-26 00:15:11 +0800

    >

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,76 @@
 import express, { Request, Response } from "express";
-import { connectDb } from "./database/database";
-import foodCategoryRouter from "./router/food.category.router";
-// import { connectDb, db } from "./database/database";
+import "dotenv/config";
+import mongoose, { model, Schema } from "mongoose";
 
 const app = express();
-app.use(express.json());
 const port = 3000;
 
-app.use("/category", foodCategoryRouter)
+app.use(express.json());
 
-// app.post("/", async (req: Request, res: Response) => {
-//   const { food } = req.body;
-//   const response = await db.collection("food").insertOne({ food });
-//   res.json(response);
-//   // res.send("Hello Bolormaa ~L")
-// });
+const url = process.env.MONGO_URI!;
 
-// app.get("/", async (req: Request, res: Response) => {
-//   const response =await db.collection("food").find().toArray();
-//   res.json(response);
-//   // res.send("Hello Bolormaa ~L")
-// });
+const connectDb = async () => {
+  try {
+    mongoose.connect(url).then(() => console.log("database connected"));
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
+
+const categoryFoodSchema = new Schema(
+  { categoryName: { type: String, default: require } },
+  { timestamps: true }
+);
+
+const FoodCategory = model("Foodcategory", categoryFoodSchema); //ene nereer ni database dotor COLLECTION UUSCH BAIGAA TIIMEES uuniig duudaj find hiine <3
+
+app.post("/", async (req: Request, res: Response) => {
+  const { categoryName } = req.body;
+
+  try {
+    const response = new FoodCategory({
+      categoryName: categoryName,
+    });
+
+    await response.save();
+    res.send({ success: true, response });
+  } catch (error) {
+    res.status(400).send({ success: false, message: "Error!" });
+    console.log(error);
+  }
+});
+
+app.get("/", async (req: Request, res: Response) => {
+  try {
+    const response = await FoodCategory.find();
+    res.send({ success: true, response });
+  } catch (error) {}
+});
+
+app.delete("/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const response = await FoodCategory.deleteOne({ _id: id });
+    res.send({ succes: true, response });
+  } catch (error) {
+    res.status(400).send({ success: false, message: "error" });
+  }
+});
+
+app.put("/", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { categoryName } = req.body;
+  try {
+    const response = await FoodCategory.updateOne(
+      { _id: id },
+      { categoryName: categoryName }
+    );
+    res.send({ success: true, response });
+  } catch (error) {
+    res.status(400).send({ success: false, message: "error" });
+  }
+});
 
 app.listen(port, async () => {
   await connectDb();
--- src/router/food.category.router.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { createFoodCategory } from "../controller/foodCategory/create-food-category";
-
-const foodCategoryRouter = express.Router();
-
-foodCategoryRouter.post("/", createFoodCategory)
-
-export default foodCategoryRouter;

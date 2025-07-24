65deae6efdc17a7c283dc2854527a40ac1be5428
Bolormaa <boloroots47@gmail.com>
2025-07-25 00:39:15 +0800

    ~L

---
+++ src/contoller/authentication/get-current-user.controller.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { User } from "../../model/user-model";
+
+export const getCurrentUser = async (req: Request, res: Response) => {
+  const authHeader = req.headers.authorization;
+  console.log("authHeader: ", authHeader);
+  const tokenToString = authHeader?.split(" ")[1] || "";
+  console.log("tokenTostring:", tokenToString);
+
+  try {
+    const secret = "WTF-SECRET-PASSWORD";
+    const isVerified = jwt.verify(tokenToString, secret) as jwt.JwtPayload;
+    console.log("isVerified: ", isVerified?.userData);
+
+    if (!isVerified) {
+      throw new Error("User not found");
+    }
+
+    const userDetail = await User.findById(isVerified.userData.userId);
+    console.log("userDetail: ", userDetail);
+
+    res.status(200).json({ user: userDetail });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send("user not found");
+  }
+};
--- src/router/user-router.ts
+++ src/router/user-router.ts
@@ -6,6 +6,7 @@ import { getUsers } from "../contoller/authentication/log-in";
 
 import { deleteuser } from "../contoller/authentication/delete-user";
 import { getAllUser } from "../contoller/authentication/getAllUser";
+import { getCurrentUser } from "../contoller/authentication/get-current-user.controller";
 
 const user = express.Router();
 
@@ -16,5 +17,7 @@ user.post("/login", getUsers);
 user.delete("/:userId", deleteuser);
 
 user.get("/", getAllUser)
+
+user.get("/get-current-user", getCurrentUser)
 
 export default user;
